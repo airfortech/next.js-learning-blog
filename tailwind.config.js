d0655b58d687dfb5e15b6dd3869369f4b088c54a
airfortech <airm@xxx.yy>
2023-01-19 15:55:21 +0100

    configured initial project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  /* definijemy foldery, w których używamy klas tailwindowych */
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
